396424bdac9cf21ec7489937c4d10ff670b4f19f
Joe Tang <63325951+joetqt@users.noreply.github.com>
2023-07-18 12:07:44 +1000

    new approach

--- learning_conf.py
+++ learning_conf.py
@@ -1,32 +1,17 @@
-import logging
-import plotly.express as px
-# Configure the logging
-logging.basicConfig(level=logging.ERROR, format='USER ENTRY: %(asctime)s - %(message)s')
-logger = logging.getLogger("error_logger")
 conf_records = []
 
 def get_confidence():
-    try:
-        # Prompt user for input
-        number = input("How confident do you feel right now on learning the content above? \n0 - Least confident; 100 - Most confident \nPlease enter an integer between 0 and 100: ")
-        # Validate input
-        while True:
-            try:
-                number = int(number)
-                if 0 <= number <= 100:
-                    break
-            except ValueError:
-                pass
-            number = input("Invalid input. Please try again. \nPlease enter an integer between 0 and 100: ")
-        # Intentionally trigger an error
-        if int(number) != -9999:
-            error_message = f"The integer you entered is: {number}"
-            # Add the number to the list
-            conf_records.append(number)
-            # Log the error message to the centralised error tracking service
-            logger.error(error_message)
-    except ValueError:
-        pass
+    # Prompt user for input
+    number = input("How confident do you feel right now on learning the content above? \n0 - Least confident; 100 - Most confident \nPlease enter an integer between 0 and 100: ")
+    # Validate input
+    while True:
+        number = int(number)
+        if 0 <= number <= 100:
+            break
+        number = input("Invalid input. Please try again. \nPlease enter an integer between 0 and 100: ")
+    print(f"The integer you entered is: {number}")
+    # Add the number to the list
+    conf_records.append(number)
     
 def plot_confidence():
     fig = px.line(y=conf_records, range_y=[0, 100])
